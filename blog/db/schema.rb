61023e22b03d0f1839f40c658a0ef444b37cfb6d
Konstantin <venberg.dev@gmail.com>
2018-06-20 15:39:24 +0300

    Added view contacts, model Contact, db:migration

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_043650) do
+ActiveRecord::Schema.define(version: 2018_06_20_123130) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "atext"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
