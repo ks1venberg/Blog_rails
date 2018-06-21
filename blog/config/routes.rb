16d71b6a98dd13956c12e8754326329365ec2cc0
ks1venberg <venberg.dev@gmail.com>
2018-06-21 06:33:54 +0300

    change get_page contacts/new to contacts

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+# get 'contacts' => 'contacts#new'
+# resource :contacts, only: [:create] 
+
 	resources :articles
-  resource :contacts, only: [:new, :create] 
+  resource :contacts, only: [:new, :create], path_names: {:new =>''}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
