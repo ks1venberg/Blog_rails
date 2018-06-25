6f386b2f8041b088edd8c1bcd4562d032ea6179b
ks1venberg <venberg.dev@gmail.com>
2018-06-25 22:10:46 +0300

    created controller Comments, added routes

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -9,7 +9,10 @@ get 'about' => 'pages#about'
 # get 'contacts' => 'contacts#new'
 # resource :contacts, only: [:create] 
 
-	resources :articles
+	resources :articles do
+    resources :comments
+  end
+  
   resource :contacts, only: [:new, :create], path_names: {:new =>''}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
