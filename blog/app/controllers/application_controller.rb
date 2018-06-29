9b1a6582e97bd12a82c84a3e5d954a1839c6f65b
ks1venberg <venberg.dev@gmail.com>
2018-06-29 08:06:35 +0300

    added devise methods, permitted username

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exeption
+  before_action :configure_permitted_parameters, if :devise_controller?
+
+
+
+private
+
+  def configure
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
+
 end
