4a20b30ce59fec35b0ed4203e42df24ed158b0a6
ks1venberg <venberg.dev@gmail.com>
2018-06-29 20:48:55 +0300

    update permit parameters for devise

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
 
-  protect_from_forgery with: :exeption
-  before_action :configure_permitted_parameters, if :devise_controller?
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 
-private
+protected
 
-  def configure
-    devise_parameter_sanitizer.for(:sign_up) << :username
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 
 end
