9e686ba83593997dfdc0b787eda289f4aeba7e4a
ks1venberg <venberg.dev@gmail.com>
2018-07-18 13:25:58 +0300

    test modified

--- blog/spec/models/contact_spec.rb
+++ blog/spec/models/contact_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
-describe "something" do
-  it "does something" do
-    #...
-  end
-end
+    RSpec.describe Contact, type: :model do
+        it { should validate_presence_of(:email) }
+        it { should validate_presence_of(:message) }
+    end
