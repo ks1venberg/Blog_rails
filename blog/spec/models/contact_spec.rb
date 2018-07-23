570159f7f6b13131cd1a95865e2c76c32a40a210
ks1venberg <venberg.dev@gmail.com>
2018-07-23 13:03:43 +0300

    cleared gemfiles, bundle install, corrected spec files

--- blog/spec/models/contact_spec.rb
+++ blog/spec/models/contact_spec.rb
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-# require "spec_helper"
-
-# describe "something"  do
-#   it "does something" do
-#     #...
-#   end
-# end
-=======
 require "rails_helper"
 
     RSpec.describe Contact, type: :model do
         it { should validate_presence_of(:email) }
         it { should validate_presence_of(:message) }
     end
->>>>>>> origin/new_step_2
