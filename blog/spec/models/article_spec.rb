0340048daa1f62fe7e9184a231de5667911cb839
ks1venberg <venberg.dev@gmail.com>
2018-07-24 11:41:35 +0300

    added validation & tests for Article.model

--- blog/spec/models/article_spec.rb
+++ blog/spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 describe Article do
+    describe "validations" do
+        it { should validate_presence_of(:title) }
+        it { should validate_presence_of(:atext) }
+    end
+
+    describe "assoсiations" do
         it { should have_many :comments }
+    end
 end
