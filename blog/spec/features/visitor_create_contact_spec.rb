f7b1c5a761a9891615e05bbb64293e8acafd198c
ks1venberg <venberg.dev@gmail.com>
2018-07-30 12:52:46 +0300

    added scenario which allows guest to add comment

--- blog/spec/features/visitor_create_contact_spec.rb
+++ blog/spec/features/visitor_create_contact_spec.rb
@@ -8,5 +8,15 @@ feature "Create_contact" do
 
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario "allow the guest to create comment" do
+    visit '/contacts'
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'something'
+    click_button 'Send message'
+
+    expect(page).to have_content 'Thanks!'
+
+  end
   
 end
