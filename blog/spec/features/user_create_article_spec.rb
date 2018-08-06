6e276c40d70cfb932eb1b1a89da86b1db37639fd
ks1venberg <venberg.dev@gmail.com>
2018-08-06 16:59:03 +0300

    Added i18n for articles pages and tests

--- blog/spec/features/user_create_article_spec.rb
+++ blog/spec/features/user_create_article_spec.rb
@@ -8,7 +8,7 @@ feature "Create_Article" do
 
   scenario "Allow user to visit new article page" do
     visit new_article_path
-    expect(page).to have_content 'New article'
+    expect(page).to have_content I18n.t('articles.new')
   end
   
 end
