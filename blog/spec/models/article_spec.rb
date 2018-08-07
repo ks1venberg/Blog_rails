a01c23e52b8f40a1603c8885e8539b8ec8ced9df
ks1venberg <venberg.dev@gmail.com>
2018-08-07 12:10:54 +0300

    added testing matchers in article_spec

--- blog/spec/models/article_spec.rb
+++ blog/spec/models/article_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 
 describe Article do
+
     describe "validations" do
         it { should validate_presence_of(:title) }
-        #it { should validate_length_of(:title), minimum: 15, on: :create }
         it { should validate_presence_of(:atext) }
+        it { should validate_length_of(:title), minimum: 15 }
     end
 
     describe "assoсiations" do
@@ -15,19 +16,19 @@ describe Article do
       it "returns the article title" do
       #create article in smart way
       article = create(:article, title: 'Lorem Ipsum')
-
+      
       #assert, проверка
       expect(article.subject).to eq 'Lorem Ipsum'
+      should have_db_column(:created_at)
       end
     end
 
     describe "#last_comment" do
       it "returns the last comment" do
         article = create(:article_with_comments)
-
         expect(article.last_comment.body).to eq "comment body 3"
+        should validate_length_of(article.last_comment.body), minimum: 15
       end
-            it { should validate_length_of(:atext), minimum: 20}
-            it { should validate_length_of(:title), minimum: 20}
-    end 
+    end
+
 end
