289d1b7cc84313571e6504cffb39a8f4681da6e7
ks1venberg <venberg.dev@gmail.com>
2018-08-07 17:11:58 +0300

    added tests to article spec

--- blog/spec/features/user_create_article_spec.rb
+++ blog/spec/features/user_create_article_spec.rb
@@ -2,13 +2,22 @@ require "rails_helper"
 
 feature "Create_Article" do
   
-  before(:all) do
+  before(:each) do
     sign_up
   end
 
   scenario "Allow user to visit new article page" do
     visit new_article_path
     expect(page).to have_content I18n.t('articles.new')
+  end
+
+  scenario "Allow user to save new article" do
+    visit new_article_path
+    fill_in :article_title, :with => 'TestTheme'
+    fill_in :article_atext, :with => 'something123'
+    click_button 'Save Article'
+    # expect(page).to have_xpath(:article_path(article))
+    #expect(page).to have_content I18n.t('articles.new')
   end
   
 end
