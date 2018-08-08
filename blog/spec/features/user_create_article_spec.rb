9a30d028d55cfe61cf5f2cefbe78c2ff3bf2bec1
ks1venberg <venberg.dev@gmail.com>
2018-08-08 19:36:02 +0300

    Article_spec added to edit_save test

--- blog/spec/features/user_create_article_spec.rb
+++ blog/spec/features/user_create_article_spec.rb
@@ -11,13 +11,18 @@ feature "Create_Article" do
     expect(page).to have_content I18n.t('articles.new')
   end
 
-  scenario "Allow user to save new article" do
+  scenario "Allow user to save new article, then edit it" do
     visit new_article_path
     fill_in :article_title, :with => 'TestTheme'
     fill_in :article_atext, :with => 'something123'
     click_button 'Save Article'
-    # expect(page).to have_xpath(:article_path(article))
-    #expect(page).to have_content I18n.t('articles.new')
+
+    visit edit_article_path 1
+      expect(page).to have_content I18n.t('articles.edit')
+      fill_in :article_atext, :with => 'TEST_EDIT_something'
+      click_button 'Save Article'
+
+    expect(page).to have_content 'TEST_EDIT_something'
   end
   
 end
